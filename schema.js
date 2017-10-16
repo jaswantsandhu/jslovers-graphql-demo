c282005ac9a4d5afa538133c67d67c6faa30f9f3
Jaswant Singh <jaswantsingh@Jaswant-Singhs-MacBook-Pro.local>
2017-10-16 11:06:35 +0530

    Clean Up

--- schema.js
+++
@@ -1,6 +0,0 @@
-const { convertToSchema } = require("jsontographql");
-const Pitchman = require("./data/Pitchman.json");
-const fs = require("fs");
-
-const schema = convertToSchema(Pitchman);
-fs.writeFile('schema/pitchman-schema.js', schema, function () {})
